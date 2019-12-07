26970de9c0be0efb20903448f1d11ecca641bd62
alexhateful <alexhateful@gmail.com>
2019-12-07 09:51:32 +0200

    add jquery

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,7 +1,10 @@
 import svg4everybody from 'svg4everybody';
+import jQuery from 'jquery';
 
 // Import polyfills
 import 'promise-polyfill/src/polyfill';
 
 // Manually initialize third-party libraries
 svg4everybody();
+
+window.$ = window.jQuery = jQuery;
